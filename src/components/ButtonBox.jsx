04c954f23f77dabf3d203b2cd954b05c6aa4b972
ananyabhtia <ananyabhtia@gmail.com>
2025-08-02 17:53:54 -0500

    styling changes

--- src/components/ButtonBox.jsx
+++ src/components/ButtonBox.jsx
@@ -2,16 +2,35 @@ const ButtonBox = () => {
     return (
         <div className="flex flex-row w-full h-1/12 border-yellow-700 rounded-xl justify-between items-center mb-1">
             <div className="flex flex-row border-orange-700 rounded-xl justify-between items-center">
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full mr-1">prev</button>
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full m-1">line number</button>
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full m-1">step number</button>
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full m-1">total steps</button>
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full m-1">next</button>
+                <div className="flex flex-row bg-purple-300 hover:bg-purple-500 font-bold py-2 px-4 rounded-full h-full mr-1 items-center">
+                    <i className="fa-solid fa-arrow-left mr-2" />
+                    <p>prev</p>
+                </div>
+                <div className="flex flex-row bg-purple-300 hover:bg-purple-500 font-bold py-2 px-4 rounded-full h-full mr-1 items-center">
+                    <p>line number</p>
+                    <input type="text" className="bg-white ml-2 text-black rounded-full pl-2 w-10"></input>
+                </div>
+                <div className="flex flex-row bg-purple-300 hover:bg-purple-500 font-bold py-2 px-4 rounded-full h-full mr-1 items-center">
+                    <p>step number</p>
+                    <input type="text" className="bg-white ml-2 text-black rounded-full pl-2 w-10"></input>
+                </div>
+                <div className="flex flex-row bg-purple-300 hover:bg-purple-500 font-bold py-2 px-4 rounded-full h-full mr-1 items-center">
+                    <p>total steps</p>
+                    <input type="text" className="bg-white ml-2 text-black rounded-full pl-2 w-10"></input>
+                </div>
+                <div className="flex flex-row bg-purple-300 hover:bg-purple-500 font-bold py-2 px-4 rounded-full h-full mr-1 items-center">
+                    <p>next</p>
+                    <i className="fa-solid fa-arrow-right ml-2" />
+                </div>
             </div>
             <div className="flex flex-row border-orange-700 rounded-xl justify-between items-center">
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full m-1">undo</button>
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full m-1">redo</button>
-                <button className="bg-gray-200 hover:bg-gray-400 font-bold py-2 px-4 rounded-full border-2 h-full ml-1">save</button>
+                <div className="bg-yellow-200 hover:bg-yellow-400 font-bold py-2 px-4 rounded-full h-full m-1">
+                    <i className="fa-solid fa-arrow-rotate-left"/>
+                </div>
+                <div className="bg-yellow-200 hover:bg-yellow-400 font-bold py-2 px-4 rounded-full h-full m-1">
+                    <i className="fa-solid fa-arrow-rotate-right"/>
+                </div>
+                <div className="bg-yellow-200 hover:bg-yellow-400 font-bold py-2 px-4 rounded-full h-full ml-1">save</div>
             </div>
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const Header = () => {
+    const [diagramName, setDiagramName] = useState("untitled-diagram");
+
     return (
-        <div className="border-red-700 flex flex-row rounded-xl mb-2">
-            <div className="bg-gray-200 w-50 h-15 border-black rounded-xl border-2 mr-2">logo</div>
-            <div className="bg-gray-200 w-200 h-15 border-black rounded-xl border-2">page title</div>
-            <div className="bg-gray-200 w-15 h-15 border-black rounded-xl border-2 ml-auto">menu</div>
+        <div className="border-red-700 flex flex-row rounded-xl mb-2 h-1/16 items-center">
+            <div className="flex flex-row w-1/6 h-full rounded-xl mr-2 items-center justify-center">
+                <h1 className="text-transparent text-3xl font-extrabold bg-clip-text bg-gradient-to-r from-orange-500 via-red-500 to-yellow-400 drop-shadow-lg">Program Tracer</h1>
+            </div>
+            <div className="flex flex-row bg-green-200 w-5/8 h-full rounded-full items-center justify-center">
+                <input type="text" className="bg-white ml-2 mr-2 text-black w-1/3 rounded-full pl-2 h-3/4 text-center" value={diagramName} onChange={e => setDiagramName(e.target.value)} />
+            </div>
+            <i className="fa-solid fa-bars fa-xl ml-auto hover:text-blue-500" />
         </div>
     );
 };
